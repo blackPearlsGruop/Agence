feca949ce725df53262ea4b3973215d2dc0a46d0
blackPearlsGruop <info@blackpearls.sa>
2026-07-27 19:12:18 +0300

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,9 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.androidApplication) apply false
+    alias(libs.plugins.jetbrainsKotlinAndroid) apply false
+    id("androidx.navigation.safeargs.kotlin") version "2.5.0" apply false
+    id("com.google.gms.google-services") version "4.3.14" apply false
+    id("com.google.firebase.crashlytics") version "2.9.1" apply false
+
+}
